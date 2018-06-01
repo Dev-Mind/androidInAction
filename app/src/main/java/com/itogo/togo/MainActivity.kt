6296f752c82eb74a12e604958909318b56ed8927
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-06-01 18:38:04 +0200

    Adds click listener on the menu items

--- app/src/main/java/com/itogo/togo/MainActivity.kt
+++ app/src/main/java/com/itogo/togo/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.itogo.togo
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        saveSpeaker.setOnClickListener{
+        saveSpeaker.setOnClickListener {
             Snackbar.make(it, "You add ${mainName.text}", Snackbar.LENGTH_LONG)
                     .setAction("Action", null)
                     .show()
@@ -22,5 +25,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuEmail -> {
+                val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:guillaume@dev-mind.fr"))
+                startActivity(intent)
+                return true
+            }
+            R.id.menuGithub -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/itogoconf/itogo.org"))
+                startActivity(intent)
+                return true
+            }
+            R.id.menuSpeakerList -> {
+                startActivity(Intent(this, SpeakerListActivity::class.java))
+                return true
+            }
+        }
+        return false
     }
 }
