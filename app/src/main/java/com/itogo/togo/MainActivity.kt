3310195a1f61f2fba22c477d9d00931c526559e9
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-06-01 18:24:18 +0200

    Update view to add name and country

--- app/src/main/java/com/itogo/togo/MainActivity.kt
+++ app/src/main/java/com/itogo/togo/MainActivity.kt
@@ -2,11 +2,19 @@ package com.itogo.togo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        saveSpeaker.setOnClickListener{
+            Snackbar.make(it, "You add ${mainName.text}", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .show()
+        }
     }
 }
