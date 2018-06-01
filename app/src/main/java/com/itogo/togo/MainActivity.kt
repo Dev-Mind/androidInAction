5b5d130770194a7c6e7c51280e1a937d346041db
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-06-01 18:32:23 +0200

    Adds menu and icons

--- app/src/main/java/com/itogo/togo/MainActivity.kt
+++ app/src/main/java/com/itogo/togo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.itogo.togo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +17,10 @@ class MainActivity : AppCompatActivity() {
                     .setAction("Action", null)
                     .show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
---
+++ app/src/main/java/com/itogo/togo/SpeakerListActivity.kt
@@ -0,0 +1,12 @@
+package com.itogo.togo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SpeakerListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speaker_list)
+    }
+}
